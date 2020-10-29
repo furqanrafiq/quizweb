1d9aca6bf0e69df1f93aef406f9bd303ff44d1b5
Furqan Rafiq <72431680+furqanrafiq@users.noreply.github.com>
2020-10-29 16:47:03 +0500

    Create Question.js

---
+++ components/Question.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Answer(props) {
+    return (
+        <div>
+            <button className="btnAnswer" type="button" onClick={() => {
+                props.handleClick(props.choice)
+            }}>{props.answer}</button>
+        </div>
+    )
+}
+
+export default Answer;
